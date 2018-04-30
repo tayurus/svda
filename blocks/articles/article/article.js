d785f72d16461dd5a8649a235bd5e7e76eb767f8
tayurus <tayurus@gmail.com>
2018-04-30 18:50:55 +0300

    add article block (not adaptive)

---
+++ blocks/articles/article/article.js
@@ -0,0 +1,36 @@
+$(".article__carousel").owlCarousel({
+    items:1,
+    nav:true,
+    dots:false,
+    navText: ["", ""]
+});
+
+$(".article .owl-prev").each(function(index,item) {
+    $(item).after("<div class='article__carousel-counter'>\
+    <span class='article__carousel-current'>1</span>\
+    /\
+    <span class='article__carousel-total'>"+$(this).parent().parent().find('.owl-stage-outer .owl-stage .owl-item').length+"</span>\
+    </div>");
+});
+
+$(".article .owl-prev").click(function() {
+    let curr = parseInt($(this).siblings().find('.article__carousel-current').text());
+    if (curr > 1)
+         $(this).siblings().find('.article__carousel-current').text(curr - 1);
+})
+
+$(".article .owl-next").click(function() {
+    let curr = parseInt($(this).siblings().find('.article__carousel-current').text());
+    let total = parseInt($(this).parent().parent().find('.owl-stage-outer .owl-stage .owl-item').length);
+    if (curr < $(this).parent().parent().find('.owl-stage-outer .owl-stage .owl-item').length)
+         $(this).siblings().find('.article__carousel-current').text(curr + 1);
+});
+
+$(".article__tab-icon").click(function() {
+    $(".article__tab").removeClass("article__tab_active");
+    $(this).parent().addClass("article__tab_active");
+
+
+    $(".article__tab-content").removeClass("article__tab-content_active");
+    $(".article__tab-content[data-tab="+$(this).attr('data-tab')+"]").addClass("article__tab-content_active");
+})
--- blocks/modal-window/modal-window.js
+++ blocks/modal-window/modal-window.js
@@ -2,6 +2,6 @@ $(".modal-window__close, .modal-wrapper").click(function() {
     $(".modal-window, .modal-wrapper").hide();
 });
 
-$(".header__backcall, .footer__backcall, .consultation .button_modal-window").click(function(){
+$(".header__backcall, .footer__backcall, .consultation .button_modal-window, .article .button_modal-window").click(function(){
     $(".modal-window, .modal-wrapper").show();
 })
