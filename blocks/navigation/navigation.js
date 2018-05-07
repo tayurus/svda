f6191557a44e7e55619a2923e0d20520f4bca478
tayurus <tayurus@gmail.com>
2018-05-07 13:56:13 +0300

    nabigation: fix position

--- blocks/navigation/navigation.js
+++ blocks/navigation/navigation.js
@@ -1,3 +1,5 @@
+$(".navigation").hide();
+
 function isInViewPort(elem) {
     var elementTop = $(elem).offset().top;
     var elementBottom = elementTop + $(elem).outerHeight();
@@ -7,22 +9,31 @@ function isInViewPort(elem) {
 };
 
 $(window).on('resize scroll', function() {
-    // if (isInViewPort($("#top")) || isInViewPort($("#bottom")))
-    //     $(".navigation__link").removeClass("navigation__link_active");
-    if (isInViewPort($("#service"))){
+    if (isInViewPort($("#top")) || isInViewPort($("#about"))){
+        $(".navigation").hide();
+    }
+    else if (isInViewPort($("#service"))){
+        console.log("service");
         $(".navigation__link").removeClass("navigation__link_active");
         $(".navigation__link[href='#service']").addClass("navigation__link_active");
+        $(".navigation").show();
     }
     else if (isInViewPort($("#advantage"))){
+        console.log("advantage");
         $(".navigation__link").removeClass("navigation__link_active");
         $(".navigation__link[href='#advantage']").addClass("navigation__link_active");
+        $(".navigation").show();
     }
     else if (isInViewPort($("#feedback"))){
+        console.log("feedback");
         $(".navigation__link").removeClass("navigation__link_active");
         $(".navigation__link[href='#feedback']").addClass("navigation__link_active");
+        $(".navigation").show();
     }
     else if (isInViewPort($("#maps"))){
+        console.log("maps");
         $(".navigation__link").removeClass("navigation__link_active");
         $(".navigation__link[href='#maps']").addClass("navigation__link_active");
+        $(".navigation").show();
     }
 });
