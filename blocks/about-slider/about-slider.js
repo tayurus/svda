22aa417d8a82bb1a38b0383780b15943c5e7882b
tayurus <tayurus@gmail.com>
2018-05-07 13:09:59 +0300

    about-slider: add js and adaptive

---
+++ blocks/about-slider/about-slider.js
@@ -0,0 +1,10 @@
+$(".about-slider__nav-item").click(function() {
+    let clickedSlide = parseInt($(this).text()) - 1;
+    let scrollTo = clickedSlide * $(".about-slider__slider").width();
+    $(".about-slider__slider").animate({scrollLeft: scrollTo}, 1000);
+
+    $(".about-slider__nav-item").removeClass("about-slider__nav-item_active");
+    $(this).addClass("about-slider__nav-item_active");
+
+
+});
