1fa0c9eda4ff892badb9219ef795cd5111916674
tayurus <tayurus@gmail.com>
2018-05-26 19:55:52 +0300

    fixes

--- build/script.js
+++ build/script.js
@@ -59,41 +59,6 @@ $(".header__backcall, .footer__backcall, .consultation .button_modal-window, .ar
     $(".modal-window, .modal-wrapper").show();
 })
 
-var currentService = 0;
-var countServices = $(".service").length;
-$(".service__slider-next").click(function(){
-    ChangeActiveService(currentService + 1);
-})
-
-$(".service__slider-prev").click(function(){
-    ChangeActiveService(currentService - 1);
-})
-
-$(".service").click(function(){
-    ChangeActiveService($(this).index());
-})
-
-function ChangeActiveService(n){
-    if(n < countServices && n >= 0){
-        $(".service").removeClass("service_active");
-        $($(".service")[n]).addClass("service_active");
-        
-        $(".container__service-content").prop("hidden", true);
-        $($(".container__service-content")[n]).prop("hidden", false);
-        
-        $(".service__slider-text").prop("hidden", true);
-        $($(".service__slider-text")[n]).prop("hidden", false);
-
-        $(".service__slider-prev, .service__slider-next").removeClass("service__slider_disabled");
-        if(n == 0){
-            $(".service__slider-prev").addClass("service__slider_disabled");
-        }
-        if(n == countServices - 1){
-            $(".service__slider-next").addClass("service__slider_disabled");   
-        }
-        currentService = n;
-    }
-}
 $(".navigation").hide();
 
 function isInViewPort(elem) {
@@ -134,6 +99,41 @@ $(window).on('resize scroll', function() {
     }
 });
 
+var currentService = 0;
+var countServices = $(".service").length;
+$(".service__slider-next").click(function(){
+    ChangeActiveService(currentService + 1);
+})
+
+$(".service__slider-prev").click(function(){
+    ChangeActiveService(currentService - 1);
+})
+
+$(".service").click(function(){
+    ChangeActiveService($(this).index());
+})
+
+function ChangeActiveService(n){
+    if(n < countServices && n >= 0){
+        $(".service").removeClass("service_active");
+        $($(".service")[n]).addClass("service_active");
+        
+        $(".container__service-content").prop("hidden", true);
+        $($(".container__service-content")[n]).prop("hidden", false);
+        
+        $(".service__slider-text").prop("hidden", true);
+        $($(".service__slider-text")[n]).prop("hidden", false);
+
+        $(".service__slider-prev, .service__slider-next").removeClass("service__slider_disabled");
+        if(n == 0){
+            $(".service__slider-prev").addClass("service__slider_disabled");
+        }
+        if(n == countServices - 1){
+            $(".service__slider-next").addClass("service__slider_disabled");   
+        }
+        currentService = n;
+    }
+}
 let owl = $(".article__carousel");
 owl.owlCarousel({
     items:1,
